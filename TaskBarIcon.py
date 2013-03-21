731b6cde7d89dff7d2aa67c22a23f8c909afbc13
Juri Semjonov <juri.semjonov@gmail.com>
2013-03-21 23:26:25 +0200

    initial commit

---
+++ TaskBarIcon.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import wx
+import os
+
+TRAY_TOOLTIP = 'System Tray Demo'
+TRAY_ICON = 'icon.png'
+
+def create_menu_item(menu, label, func):
+    item = wx.MenuItem(menu, -1, label)
+    menu.Bind(wx.EVT_MENU, func, id=item.GetId())
+    menu.AppendItem(item)
+    return item
+
+class TaskBarIcon(wx.TaskBarIcon):
+    def __init__(self):
+        super(TaskBarIcon, self).__init__()
+        self.set_icon(TRAY_ICON)
+        self.Bind(wx.EVT_TASKBAR_LEFT_DOWN, self.on_left_down)
+    def CreatePopupMenu(self):
+        menu = wx.Menu()
+        create_menu_item(menu, 'Launch Photry Website', self.on_hello)
+        menu.AppendSeparator()
+        create_menu_item(menu, 'Exit', self.on_exit)
+        return menu
+    def set_icon(self, path):
+        icon = wx.IconFromBitmap(wx.Bitmap(path))
+        self.SetIcon(icon, TRAY_TOOLTIP)
+    def on_left_down(self, event):
+        print 'Tray icon was left-clicked.pho'
+    def on_hello(self, event):
+        print 'Hello, world!'
+    def on_exit(self, event):
+        wx.CallAfter(self.Destroy)
+
+def main():
+    app = wx.PySimpleApp()
+    TaskBarIcon()
+    app.MainLoop()
+
+if __name__ == '__main__':
+    main()
